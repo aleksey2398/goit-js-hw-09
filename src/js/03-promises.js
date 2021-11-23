0482a8fffc55c93faa147c8692a09f16b7ab3392
aleksey2398 <87311512+aleksey2398@users.noreply.github.com>
2021-11-23 13:16:48 +0200

    update 3 task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,28 +1,35 @@
-import { Notify } from "notiflix";
+
+import { Notify } from 'notiflix';
 
 const refs = {
- form : document.querySelector('.form'),
- startDelay : document.querySelector('input[name=delay]'),
- step : document.querySelector('input[name=step]'),
- amount : document.querySelector('input[name=amount]'),
-}
+  form: document.querySelector('.form'),
+  startDelay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+};
 
 refs.form.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
   let delay = Number(refs.startDelay.value);
-  for (let i = 0; i < Number(refs.amount.value); i++) {
-    const position = i+1;
-    delay += Number(refs.step.value);
+
+  for (let position = 1; position <= Number(refs.amount.value); position += 1) {
     createPromise(position, delay)
-    .then(({ position, delay }) => {
-      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`,{useIcon: false, clickToClose: true});
-    })
-    .catch(({ position, delay }) => {
-      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`,{useIcon: false, clickToClose: true});
-    });
-    
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+          useIcon: false,
+          clickToClose: true,
+        });
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+          useIcon: false,
+          clickToClose: true,
+        });
+      });
+
+    delay += Number(refs.step.value);
   }
 }
 
